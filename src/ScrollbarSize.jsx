45295187dac3835cef570877c3951ecd0b095857
Shawn McKnight <smcknight@storis.com>
2017-04-16 17:13:07 -0400

    Squashed commit of the following:
    commit e5c730f62b8712337aab5a2341c27b18dc3a6392
    Author: Shawn McKnight <smcknight@storis.com>
    Date:   Sun Apr 16 17:12:45 2017 -0400
    
        Update to prop-types module
        Additionally fixed lingering CRLF --> LF issues

--- src/ScrollbarSize.jsx
+++ src/ScrollbarSize.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import EventListener from 'react-event-listener';
 import isEqual from 'lodash.isequal';
 
