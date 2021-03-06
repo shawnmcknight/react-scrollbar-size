49db2bcf66cdab2276de03d12460158e45e555e4
Shawn McKnight <smcknight@storis.com>
2021-03-06 17:06:18 -0500

    Create new example with hook

---
+++ example/ScrollbarSizeDemo.tsx
@@ -0,0 +1,25 @@
+import React, { CSSProperties, FunctionComponent } from 'react';
+import { useScrollbarSize } from '../src';
+
+const styles: CSSProperties = {
+	margin: '1rem',
+	textAlign: 'center',
+};
+
+const ScrollbarSizeDemo: FunctionComponent = () => {
+	const { height, width } = useScrollbarSize();
+
+	return (
+		<div style={styles}>
+			<h2>React-Scrollbar-Size Component Example</h2>
+			<h4>Tip: Change browser zoom level to see scrollbar sizes change.</h4>
+			<p>
+				{`The current height of the scrollbar is ${height}px.`}
+				<br />
+				{`The current width of the scrollbar is ${width}px.`}
+			</p>
+		</div>
+	);
+};
+
+export default ScrollbarSizeDemo;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import ScrollbarSize from './ScrollbarSize';
+import useScrollbarSize from './useScrollbarSize';
 
 export type { ScrollbarSizeChangeHandlerParams, ScrollbarSizeProps } from './ScrollbarSize';
 
+export { useScrollbarSize };
 export default ScrollbarSize;
