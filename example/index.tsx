0f70841f63bbf8918dfd53905674a84597e4e9ee
Shawn McKnight <smcknight@storis.com>
2022-04-30 10:10:49 -0400

    Add testing-library/react to branch Needed to resolve peer dependency issues and could not update react and testing-library/react independently Additionally updated to use createRoot API in Example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ScrollbarSizeDemo from './ScrollbarSizeDemo';
 import 'normalize.css';
 import './main.css';
 
-render(<ScrollbarSizeDemo />, document.getElementById('root'));
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(<ScrollbarSizeDemo />);
